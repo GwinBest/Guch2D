4a274c938d992de4811e506ac8163d8b76d802e5
Matvey <thegwinbest@gmail.com>
2025-11-23 18:17:27 +0300

    feat: Add main window

--- Demo/Main.cpp
+++ Demo/Main.cpp
@@ -1,5 +1,21 @@
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    auto window = sf::RenderWindow(sf::VideoMode({1280, 720}), "Guch2D");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.display();
+    }
     return 0;
 }
