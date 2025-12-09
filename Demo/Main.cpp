e96b65d42f6aa3c24d4c5611a891f20ab8203d21
Matvey <thegwinbest@gmail.com>
2025-12-09 14:11:58 +0300

    fix: Include paths fix

--- Demo/Main.cpp
+++ Demo/Main.cpp
@@ -17,5 +17,6 @@ int main()
 
         window.display();
     }
+
     return 0;
 }
---
+++ Src/Dynamics/DynamicWorld.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Math/Vector.hpp"
+
+namespace Guch2D
+{
+    class DynamicWorld
+    {
+    private:
+        Vect _gravity = Vect(0.0F, 0.91F);
+    };
+}
