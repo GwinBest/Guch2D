9cfc5eaf3bef4426f7ac64e7e52b6555d849d371
Matvey <thegwinbest@gmail.com>
2025-12-11 15:20:43 +0300

    feat: Add circle collider

---
+++ Src/Collision/CircleCollider.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Collision/Collider.hpp"
+
+namespace Guch2D
+{
+    class CircleCollider final : public Collider
+    {
+    public:
+        CircleCollider() noexcept = default;
+        CircleCollider(const CircleCollider&) = default;
+        CircleCollider(CircleCollider&&) = default;
+        CircleCollider& operator=(const CircleCollider&) = default;
+        CircleCollider& operator=(CircleCollider&&) = default;
+        ~CircleCollider() override = default;
+
+        [[nodiscard]] constexpr float GetRadius() const noexcept { return _radius; }
+
+        constexpr void SetRadius(const float radius) noexcept
+        {
+            if (_radius < 0.0F)
+            {
+                _radius = 0.0F;
+                return;
+            }
+
+            _radius = radius;
+        }
+
+    private:
+        // Radius of the circle collider, in meters (m)
+        float _radius = 0.0F;
+    };
+}
