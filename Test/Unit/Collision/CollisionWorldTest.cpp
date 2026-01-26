0964500384db694402e8fa837e2c23629a9a19f7
Matvey <thegwinbest@gmail.com>
2026-01-26 16:39:50 +0300

    test: Add tests for Step with circle collider

--- Test/Unit/Collision/CollisionWorldTest.cpp
+++ Test/Unit/Collision/CollisionWorldTest.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include "Collision/CircleCollider.hpp"
+
 namespace
 {
     TEST(CollisionWorldTest, DefaultConstructor)
@@ -96,4 +98,313 @@ namespace
         world.SetTimeStep(NAN);
         EXPECT_FLOAT_EQ(world.GetTimeStep(), Guch2D::CollisionWorld::DefaultTimeStep);
     }
+
+    TEST(CollisionWorldTest, StepNoObjects)
+    {
+        const Guch2D::CollisionWorld world;
+        world.Step();
+        SUCCEED();
+    }
+
+    TEST(CollisionWorldTest, StepOneCircleNoCollision)
+    {
+        Guch2D::CollisionWorld world;
+        const auto body = std::make_shared<Guch2D::CollisionBody>();
+        body->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+
+        bool onBeginOverlapCalled = false;
+        bool onEndOverlapCalled = false;
+        body->BindOnBeginOverlap([&](const Guch2D::Collision&) { onBeginOverlapCalled = true; });
+        body->BindOnEndOverlap([&](const Guch2D::Collision&) { onEndOverlapCalled = true; });
+
+        world.AddObject(body);
+        world.Step();
+
+        EXPECT_FALSE(onBeginOverlapCalled);
+        EXPECT_FALSE(onEndOverlapCalled);
+    }
+
+    TEST(CollisionWorldTest, StepTwoCirclesNoCollision)
+    {
+        Guch2D::CollisionWorld world;
+
+        const auto bodyA = std::make_shared<Guch2D::CollisionBody>();
+        bodyA->SetPosition({0.0F, 0.0F});
+        bodyA->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyA->GetCollider())->SetRadius(0.5F);
+
+        const auto bodyB = std::make_shared<Guch2D::CollisionBody>();
+        bodyB->SetPosition({5.0F, 5.0F});
+        bodyB->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyB->GetCollider())->SetRadius(0.5F);
+
+        bool onBeginOverlapCalledA = false;
+        bool onEndOverlapCalledA = false;
+        bodyA->BindOnBeginOverlap([&](const Guch2D::Collision&) { onBeginOverlapCalledA = true; });
+        bodyA->BindOnEndOverlap([&](const Guch2D::Collision&) { onEndOverlapCalledA = true; });
+
+        bool onBeginOverlapCalledB = false;
+        bool onEndOverlapCalledB = false;
+        bodyB->BindOnBeginOverlap([&](const Guch2D::Collision&) { onBeginOverlapCalledB = true; });
+        bodyB->BindOnEndOverlap([&](const Guch2D::Collision&) { onEndOverlapCalledB = true; });
+
+        world.AddObject(bodyA);
+        world.AddObject(bodyB);
+        world.Step();
+
+        EXPECT_FALSE(onBeginOverlapCalledA);
+        EXPECT_FALSE(onEndOverlapCalledA);
+        EXPECT_FALSE(onBeginOverlapCalledB);
+        EXPECT_FALSE(onEndOverlapCalledB);
+    }
+
+    TEST(CollisionWorld, StepTwoCirclesWithCollision)
+    {
+        Guch2D::CollisionWorld world;
+
+        const auto bodyA = std::make_shared<Guch2D::CollisionBody>();
+        bodyA->SetPosition({0.0F, 0.0F});
+        bodyA->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyA->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyB = std::make_shared<Guch2D::CollisionBody>();
+        bodyB->SetPosition({4.0F, 0.0F});
+        bodyB->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyB->GetCollider())->SetRadius(2.0F);
+
+        bool onBeginOverlapCalledA = false;
+        bool onEndOverlapCalledA = false;
+        bodyA->BindOnBeginOverlap([&](const Guch2D::Collision&) { onBeginOverlapCalledA = true; });
+        bodyA->BindOnEndOverlap([&](const Guch2D::Collision&) { onEndOverlapCalledA = true; });
+
+        bool onBeginOverlapCalledB = false;
+        bool onEndOverlapCalledB = false;
+        bodyB->BindOnBeginOverlap([&](const Guch2D::Collision&) { onBeginOverlapCalledB = true; });
+        bodyB->BindOnEndOverlap([&](const Guch2D::Collision&) { onEndOverlapCalledB = true; });
+
+        world.AddObject(bodyA);
+        world.AddObject(bodyB);
+        world.Step();
+
+        EXPECT_TRUE(onBeginOverlapCalledA);
+        EXPECT_FALSE(onEndOverlapCalledA);
+        EXPECT_TRUE(onBeginOverlapCalledB);
+        EXPECT_FALSE(onEndOverlapCalledB);
+    }
+
+    TEST(CollisionWorld, StepTwoCirclesCollisionBeginEnd)
+    {
+        Guch2D::CollisionWorld world;
+
+        const auto bodyA = std::make_shared<Guch2D::CollisionBody>();
+        bodyA->SetPosition({0.0F, 0.0F});
+        bodyA->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyA->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyB = std::make_shared<Guch2D::CollisionBody>();
+        bodyB->SetPosition({4.0F, 0.0F});
+        bodyB->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyB->GetCollider())->SetRadius(2.0F);
+
+        bool onBeginOverlapCalledA = false;
+        bool onEndOverlapCalledA = false;
+        bodyA->BindOnBeginOverlap([&](const Guch2D::Collision&) { onBeginOverlapCalledA = true; });
+        bodyA->BindOnEndOverlap([&](const Guch2D::Collision&) { onEndOverlapCalledA = true; });
+
+        bool onBeginOverlapCalledB = false;
+        bool onEndOverlapCalledB = false;
+        bodyB->BindOnBeginOverlap([&](const Guch2D::Collision&) { onBeginOverlapCalledB = true; });
+        bodyB->BindOnEndOverlap([&](const Guch2D::Collision&) { onEndOverlapCalledB = true; });
+
+        world.AddObject(bodyA);
+        world.AddObject(bodyB);
+        world.Step();   // Begin overlap
+
+        EXPECT_TRUE(onBeginOverlapCalledA);
+        EXPECT_FALSE(onEndOverlapCalledA);
+        EXPECT_TRUE(onBeginOverlapCalledB);
+        EXPECT_FALSE(onEndOverlapCalledB);
+
+        // Move bodyB away to end overlap
+        bodyB->SetPosition({4.1F, 0.0F});
+        world.Step();   // End overlap
+
+        EXPECT_TRUE(onBeginOverlapCalledA);
+        EXPECT_TRUE(onEndOverlapCalledA);
+        EXPECT_TRUE(onBeginOverlapCalledB);
+        EXPECT_TRUE(onEndOverlapCalledB);
+    }
+
+    TEST(CollisionWorld, StepThreeCirclesMultipleCollisions)
+    {
+        Guch2D::CollisionWorld world;
+
+        const auto bodyA = std::make_shared<Guch2D::CollisionBody>();
+        bodyA->SetPosition({0.0F, 0.0F});
+        bodyA->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyA->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyB = std::make_shared<Guch2D::CollisionBody>();
+        bodyB->SetPosition({4.0F, 0.0F});
+        bodyB->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyB->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyC = std::make_shared<Guch2D::CollisionBody>();
+        bodyC->SetPosition({2.0F, 2.0F});
+        bodyC->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyC->GetCollider())->SetRadius(2.0F);
+
+        int beginOverlapCountA = 0;
+        int endOverlapCountA = 0;
+        bodyA->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountA++; });
+        bodyA->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountA++; });
+
+        int beginOverlapCountB = 0;
+        int endOverlapCountB = 0;
+        bodyB->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountB++; });
+        bodyB->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountB++; });
+
+        int beginOverlapCountC = 0;
+        int endOverlapCountC = 0;
+        bodyC->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountC++; });
+        bodyC->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountC++; });
+
+        world.AddObject(bodyA);
+        world.AddObject(bodyB);
+        world.AddObject(bodyC);
+
+        world.Step();
+
+        EXPECT_EQ(beginOverlapCountA, 2);
+        EXPECT_EQ(endOverlapCountA, 0);
+        EXPECT_EQ(beginOverlapCountB, 2);
+        EXPECT_EQ(endOverlapCountB, 0);
+        EXPECT_EQ(beginOverlapCountC, 2);
+        EXPECT_EQ(endOverlapCountC, 0);
+    }
+
+    TEST(CollisionWorld, StepThreeCirclesMultipleCollisionsEnd)
+    {
+        Guch2D::CollisionWorld world;
+
+        const auto bodyA = std::make_shared<Guch2D::CollisionBody>();
+        bodyA->SetPosition({0.0F, 0.0F});
+        bodyA->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyA->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyB = std::make_shared<Guch2D::CollisionBody>();
+        bodyB->SetPosition({4.0F, 0.0F});
+        bodyB->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyB->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyC = std::make_shared<Guch2D::CollisionBody>();
+        bodyC->SetPosition({2.0F, 2.0F});
+        bodyC->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyC->GetCollider())->SetRadius(2.0F);
+
+        int beginOverlapCountA = 0;
+        int endOverlapCountA = 0;
+        bodyA->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountA++; });
+        bodyA->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountA++; });
+
+        int beginOverlapCountB = 0;
+        int endOverlapCountB = 0;
+        bodyB->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountB++; });
+        bodyB->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountB++; });
+
+        int beginOverlapCountC = 0;
+        int endOverlapCountC = 0;
+        bodyC->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountC++; });
+        bodyC->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountC++; });
+
+        world.AddObject(bodyA);
+        world.AddObject(bodyB);
+        world.AddObject(bodyC);
+
+        world.Step();
+
+        EXPECT_EQ(beginOverlapCountA, 2);
+        EXPECT_EQ(endOverlapCountA, 0);
+        EXPECT_EQ(beginOverlapCountB, 2);
+        EXPECT_EQ(endOverlapCountB, 0);
+        EXPECT_EQ(beginOverlapCountC, 2);
+        EXPECT_EQ(endOverlapCountC, 0);
+
+        // Move bodies apart to end all overlaps
+        bodyA->SetPosition({-10.0F, -10.0F});
+        bodyB->SetPosition({10.0F, 10.0F});
+        bodyC->SetPosition({10.0F, -10.0F});
+
+        world.Step();
+
+        EXPECT_EQ(beginOverlapCountA, 2);
+        EXPECT_EQ(endOverlapCountA, 2);
+        EXPECT_EQ(beginOverlapCountB, 2);
+        EXPECT_EQ(endOverlapCountB, 2);
+        EXPECT_EQ(beginOverlapCountC, 2);
+        EXPECT_EQ(endOverlapCountC, 2);
+    }
+
+    TEST(CollisionWorld, StepThreeCirclesWithOneCollision)
+    {
+        Guch2D::CollisionWorld world;
+
+        const auto bodyA = std::make_shared<Guch2D::CollisionBody>();
+        bodyA->SetPosition({0.0F, 0.0F});
+        bodyA->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyA->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyB = std::make_shared<Guch2D::CollisionBody>();
+        bodyB->SetPosition({4.0F, 0.0F});
+        bodyB->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyB->GetCollider())->SetRadius(2.0F);
+
+        const auto bodyC = std::make_shared<Guch2D::CollisionBody>();
+        bodyC->SetPosition({50.0F, 5.0F});
+        bodyC->SetCollider(std::make_shared<Guch2D::CircleCollider>());
+        std::dynamic_pointer_cast<Guch2D::CircleCollider>(bodyC->GetCollider())->SetRadius(2.0F);
+
+        int beginOverlapCountA = 0;
+        int endOverlapCountA = 0;
+        bodyA->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountA++; });
+        bodyA->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountA++; });
+
+        int beginOverlapCountB = 0;
+        int endOverlapCountB = 0;
+        bodyB->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountB++; });
+        bodyB->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountB++; });
+
+        int beginOverlapCountC = 0;
+        int endOverlapCountC = 0;
+        bodyC->BindOnBeginOverlap([&](const Guch2D::Collision&) { beginOverlapCountC++; });
+        bodyC->BindOnEndOverlap([&](const Guch2D::Collision&) { endOverlapCountC++; });
+
+        world.AddObject(bodyA);
+        world.AddObject(bodyB);
+        world.AddObject(bodyC);
+
+        world.Step();
+
+        EXPECT_EQ(beginOverlapCountA, 1);
+        EXPECT_EQ(endOverlapCountA, 0);
+        EXPECT_EQ(beginOverlapCountB, 1);
+        EXPECT_EQ(endOverlapCountB, 0);
+        EXPECT_EQ(beginOverlapCountC, 0);
+        EXPECT_EQ(endOverlapCountC, 0);
+
+        // Move bodies apart to end all overlaps
+        bodyA->SetPosition({-10.0F, -10.0F});
+        bodyB->SetPosition({10.0F, 10.0F});
+        bodyC->SetPosition({10.0F, -10.0F});
+
+        world.Step();
+
+        EXPECT_EQ(beginOverlapCountA, 1);
+        EXPECT_EQ(endOverlapCountA, 1);
+        EXPECT_EQ(beginOverlapCountB, 1);
+        EXPECT_EQ(endOverlapCountB, 1);
+        EXPECT_EQ(beginOverlapCountC, 0);
+        EXPECT_EQ(endOverlapCountC, 0);
+    }
+
 }   // namespace
