6eed77bc793e3ab8940ca9acf0c8c1de6f4f9b1a
Matvey <thegwinbest@gmail.com>
2026-02-09 01:31:11 +0300

    test: Add ASan bug for test

--- Test/Unit/Collision/CollisionWorldTest.cpp
+++ Test/Unit/Collision/CollisionWorldTest.cpp
@@ -673,5 +673,9 @@ namespace
                     || (bodyA->GetPosition().x == 3.0F && bodyB->GetPosition().x == -1.0F));
         EXPECT_FLOAT_EQ(bodyA->GetPosition().y, 1.0F);
         EXPECT_FLOAT_EQ(bodyB->GetPosition().y, 1.0F);
+
+        int* p1 = new int(1);
+        delete p1;
+        *p1 = 2;   // use-after-free
     }
 }   // namespace
