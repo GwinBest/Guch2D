8f2348991ad8ebc601b27ad8405ae3383849838b
Matvey <thegwinbest@gmail.com>
2026-04-25 17:38:28 +0300

    feat: add density

--- Test/Unit/Dynamics/RigidBodyTest.cpp
+++ Test/Unit/Dynamics/RigidBodyTest.cpp
@@ -1,5 +1,10 @@
 #include "Dynamics/RigidBody.hpp"
+#include "Collision/AABBCollider.hpp"
+#include "Collision/CircleCollider.hpp"
 
+#include <cmath>
+#include <memory>
+#include <numbers>
 #include <gtest/gtest.h>
 
 namespace
@@ -64,5 +69,111 @@ namespace
         Guch2D::RigidBody body;
         body.SetMass(INFINITY);
         EXPECT_EQ(body.GetMass(), 0.0F);
+    }
+
+    TEST(RigidBodyTest, SetDensityWithoutColliderKeepsMassZero)
+    {
+        Guch2D::RigidBody body;
+
+        body.SetDensity(4.5F);
+
+        EXPECT_FLOAT_EQ(body.GetDensity(), 4.5F);
+        EXPECT_FLOAT_EQ(body.GetMass(), 0.0F);
+    }
+
+    TEST(RigidBodyTest, SetDensityWithCircleColliderComputesMassFromArea)
+    {
+        Guch2D::RigidBody body;
+        auto collider = std::make_shared<Guch2D::CircleCollider>(2.0F);
+        body.SetCollider(collider);
+
+        body.SetDensity(3.0F);
+
+        EXPECT_FLOAT_EQ(body.GetDensity(), 3.0F);
+        EXPECT_NEAR(body.GetMass(), 3.0F * collider->GetArea(), 1.0e-5F);
+    }
+
+    TEST(RigidBodyTest, MassFromDensityUpdatesWhenCircleRadiusChanges)
+    {
+        Guch2D::RigidBody body;
+        auto collider = std::make_shared<Guch2D::CircleCollider>(1.0F);
+        body.SetCollider(collider);
+        body.SetDensity(2.0F);
+
+        const float initialMass = body.GetMass();
+        collider->SetRadius(3.0F);
+
+        EXPECT_NEAR(initialMass, 2.0F * std::numbers::pi_v<float>, 1.0e-5F);
+        EXPECT_NEAR(body.GetMass(), 2.0F * collider->GetArea(), 1.0e-5F);
+    }
+
+    TEST(RigidBodyTest, MassFromDensityUpdatesWhenAABBExtentChanges)
+    {
+        Guch2D::RigidBody body;
+        auto collider = std::make_shared<Guch2D::AABBCollider>(Guch2D::Vect {1.0F, 2.0F});
+        body.SetCollider(collider);
+        body.SetDensity(1.25F);
+
+        const float initialMass = body.GetMass();
+        collider->SetExtent(Guch2D::Vect {3.0F, 4.0F});
+
+        EXPECT_FLOAT_EQ(initialMass, 1.25F * 8.0F);
+        EXPECT_FLOAT_EQ(body.GetMass(), 1.25F * collider->GetArea());
+    }
+
+    TEST(RigidBodyTest, MassFromDensityUpdatesWhenColliderIsReplaced)
+    {
+        Guch2D::RigidBody body;
+        body.SetCollider(std::make_shared<Guch2D::CircleCollider>(1.0F));
+        body.SetDensity(2.0F);
+
+        const float circleMass = body.GetMass();
+
+        const auto aabbCollider = std::make_shared<Guch2D::AABBCollider>(Guch2D::Vect {2.0F, 3.0F});
+        body.SetCollider(aabbCollider);
+
+        EXPECT_NEAR(circleMass, 2.0F * std::numbers::pi_v<float>, 1.0e-5F);
+        EXPECT_FLOAT_EQ(body.GetMass(), 2.0F * aabbCollider->GetArea());
+    }
+
+    TEST(RigidBodyTest, MassFromDensityBecomesZeroWhenColliderRemoved)
+    {
+        Guch2D::RigidBody body;
+        body.SetCollider(std::make_shared<Guch2D::CircleCollider>(2.0F));
+        body.SetDensity(2.0F);
+
+        EXPECT_GT(body.GetMass(), 0.0F);
+
+        body.RemoveCollider();
+        EXPECT_FLOAT_EQ(body.GetMass(), 0.0F);
+    }
+
+    TEST(RigidBodyTest, SetMassSwitchesToManualModeAndClearsDensity)
+    {
+        Guch2D::RigidBody body;
+        auto collider = std::make_shared<Guch2D::CircleCollider>(2.0F);
+        body.SetCollider(collider);
+        body.SetDensity(5.0F);
+
+        body.SetMass(7.0F);
+        collider->SetRadius(5.0F);
+
+        EXPECT_FLOAT_EQ(body.GetDensity(), 0.0F);
+        EXPECT_FLOAT_EQ(body.GetMass(), 7.0F);
+    }
+
+    TEST(RigidBodyTest, SetInvalidDensityDisablesDensityModeAndRestoresManualMass)
+    {
+        Guch2D::RigidBody body;
+        auto collider = std::make_shared<Guch2D::CircleCollider>(2.0F);
+        body.SetCollider(collider);
+        body.SetMass(9.0F);
+        body.SetDensity(4.0F);
+
+        EXPECT_NE(body.GetMass(), 9.0F);
+
+        body.SetDensity(0.0F);
+        EXPECT_FLOAT_EQ(body.GetDensity(), 0.0F);
+        EXPECT_FLOAT_EQ(body.GetMass(), 9.0F);
     }
 }   // namespace
